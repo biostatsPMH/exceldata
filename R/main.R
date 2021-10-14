02a7c90b29da183cb5413ccd1176e8d8c3fa4c45
Lisa Avery <lisa.avery@uhnresearch.ca>
2021-10-14 17:01:50 -0400

    Added the ability to create combined variables from checkbox style survey questions. These will be plotted as pareto graphs to show the most common patterns. Still need a more useful means of communicating what the patterns mean.

--- R/main.R
+++ R/main.R
@@ -201,7 +201,7 @@ readExcelData <- function(excelFile,dataDictionary,dataSheet='DataEntry',saveWar
   # assign date types to date fields
   for (v in dataDictionary$VariableName[dataDictionary$Type=='date']) dat[[v]] <- as.Date(dat[[v]])
 
-    # Set all out of range entries to missing if specified
+  # Set all out of range entries to missing if specified
   if (setErrorsMissing){
 
     # Range checks
@@ -438,10 +438,8 @@ testForNumeric <- function(str){
 #'
 #' @param labelStr in the format code1=label1,code2=label2
 #' @param delim delimeter separating codes in labelStr, defaults to ','
-#' @param codeName column name for codes, defaults to code
-#' @param lblName column name for labels, defaults to label
 #' @export
-importCodes<-function(labelStr,delim=',',codeName='code',lblName='label'){
+importCodes<-function(labelStr,delim=','){
   x=strsplit(labelStr,split=delim)[[1]]
   # check for multiple '=', probably indicates an error
   check = sapply(x,function(y){ifelse(nchar(gsub('=','',y))+1 < nchar(y),1,0)})
@@ -451,7 +449,9 @@ importCodes<-function(labelStr,delim=',',codeName='code',lblName='label'){
   for (i in seq_along(codeLst)) tbl<-rbind(tbl,cbind(code=codeLst[[i]][1],label=codeLst[[i]][2]))
   tbl <- as.data.frame(tbl)
   if (isTRUE(all.equal(as.character(suppressWarnings(as.numeric(tbl[[1]]))),tbl[[1]]))) tbl[[1]] <- as.numeric(tbl[[1]])
-  names(tbl)=c(codeName,lblName)
+  names(tbl)=c('code','label')
+  tbl[['code']] <- sapply(tbl[['code']],function(x) trimws(x))
+  tbl[['label']] <- sapply(tbl[['label']],function(x) trimws(x))
   return(tbl)
 }
 
@@ -461,7 +461,8 @@ importCodes<-function(labelStr,delim=',',codeName='code',lblName='label'){
 #' rules in the DataDictionary.xlsm file. See the Example sheet for an example.
 #'
 #' @param data is data returned by the importExcelData or readExcelData functions
-#' @param dict is the data dictionary returned by importExcelData or readDataDict functions
+#' @param dictTable is the data dictionary returned by importExcelData or readDataDict functions
+#' @param timeUnit a string containing the desired unit of time for survival variables
 #' @export
 createCalculated<-function(data,dictTable,timeUnit='month'){
   calcVars <- dictTable$VariableName[dictTable$Type=='calculated']
@@ -493,7 +494,21 @@ createCalculated<-function(data,dictTable,timeUnit='month'){
         }} else{
           data <- createSurvVar(data,newVarName,survVars,timeUnit)
         }
+    } else if (substr(dictTable$Levels[dictTable$VariableName==v],1,7)=='combine') {
+      varsToCombine <- unlist(strsplit(trimws(gsub('combine|to','',instructions[1])),' '))
+      varsToCombine <- varsToCombine[varsToCombine!=""]
+      if (!all(varsToCombine %in% names(data))) {
+        warning(paste(paste(varsToCombine[!varsToCombine %in% names(data)],collapse=','),' not in the data. Variable names are case sensitive.\n',newVarName,' not created.'))
+      } else if (length(varsToCombine)!=2) {
+        warning(paste0(newVarName,' not created, check instructions. Must be in the format: combine var1 to var2, response=value'))
+      }  else if (substr(instructions[2],1,8)!='response'){
+        warning(paste0(newVarName,' not created, check instructions. Must be in the format: combine var1 to var2, response=value'))
+      } else{
+        responseVal <- trimws(gsub('response|=','',instructions[2]))
+        data <- createCombinedVar(data,dictTable,newVarName,varsToCombine,responseVal)
+      }
     } else {
+
       if (!instructions[1] %in% names(data)) {
         warning(paste(instructions[1],' not in the data. Variable names are case sensitive.\n',newVarName,' not created.'))
       } else {
@@ -501,7 +516,7 @@ createCalculated<-function(data,dictTable,timeUnit='month'){
           data <- createCategorisedVar(data,newVarName,instructions)
 
         } else{
-          data <- createRecodedVar(data,newVarName,instructions)
+          data <- createRecodedVar(data,dictTable,newVarName,instructions)
         }
       }
     }
@@ -562,20 +577,35 @@ createSurvVar <- function(data,newVarName,survVars,timeUnit='month'){
 #' instructions="T_Stage,T0=T0,T1up=T1,T2,T3,T4" will recode T1-T4 as T1up and retain T0
 #'
 #' @param data is data returned by the importExcelData or readExcelData functions
+#' @param dictTable is the data dictionary returned by importExcelData or readDataDict functions
 #' @param newVarName the name of the new variable.
 #' @param instructions are from the data dictionary
-createRecodedVar <- function(data,newVarName,instructions){
+createRecodedVar <- function(data,dictTable,newVarName,instructions){
 
   originalVar <- instructions[1]
 
   codeReps <-diff(  c(grep("=",instructions),length(instructions)+1))
   newCodes <- sapply(instructions[grep("=",instructions)],function(x) trimws(unlist(strsplit(x,"="))[1]))
+  oldCodes=gsub('.*=','',instructions[-1])
   labels = unlist(mapply(rep,x=newCodes,times=codeReps))
 
-  oldCodes <- gsub('.*=','',instructions[-1])
 
-  recoded = droplevels(factor(data[[originalVar]],levels = oldCodes,labels=labels))
+  # If the variable is a codes variable, then we need the original entered levels
+  if ('factor' %in% class(data[[originalVar]])) {
+    factorLevels = try(importCodes(dictTable[["Levels"]][dictTable[['VariableName']]==originalVar]),silent = T)
 
+    if (class(factorLevels)[1]=='try-error'){
+      warning(paste('Data codes for',originalVar,'could not be extracted,', newVarName,'not created.'))
+    } else {
+
+      oldCodes <- data.frame(oldCodes=oldCodes)
+      codeLookup <- merge(oldCodes,factorLevels,by.x = "oldCodes", by.y = "code" )
+      recoded = droplevels(factor(data[[originalVar]],levels = codeLookup[,2],labels=labels))
+    }
+  } else{
+    recoded = droplevels(factor(data[[originalVar]],levels = oldCodes,labels=labels))
+
+  }
   # Add the variable to the data
   data[[newVarName]] <- recoded
 
@@ -583,6 +613,90 @@ createRecodedVar <- function(data,newVarName,instructions){
   tbl_check <- capture.output(table(data[[newVarName]],data[[originalVar]]))
   WriteToLog(tbl_check)
 
+  return(data)
+}
+
+#' Create a combined variable from several dichotomous variables
+#'
+#' This function will create a single variable from a set of dichotomous
+#' variables, usually checkbox items from a survey. The combined variable may be
+#' if there are a small number of popular response patterns. Currently this
+#' function only works with dichotomous variables.
+#'
+#' The instructions are contained in the Levels column of the data dictionary and should be in the format:
+#' original_varname,newCode1=oldcode1,oldCode2,...,newCode2=oldCode3,..
+#' For Example:
+#' instructions="T_Stage,T0=T0,T1up=T1,T2,T3,T4" will recode T1-T4 as T1up and retain T0
+#'
+#' @param data is data returned by the importExcelData or readExcelData functions
+#' @param dictTable is the data dictionary returned by importExcelData or readDataDict functions
+#' @param newVarName the name of the new variable.
+#' @param varsToCombine a vector of the first and last variables to combine into the new variable. Note that the variables to be conbined mut be contiguous in the data.
+#' @param responseVal the value of the variables to be combined, usually this will be 1 for 0,1 variables or Yes for Yes No or Checked for Checked Unchecked
+createCombinedVar <- function(data,dictTable,newVarName,varsToCombine,responseVal){
+
+  startVar = which(names(data)==varsToCombine[1])
+  endVar = which(names(data)==varsToCombine[2])
+  var_dat <- data[,startVar:endVar]
+
+  # determine if data has already been turned into factors, or if it is numeric.
+  # If all columns are not the same time warning user and don't create the variable
+  types <- unlist(lapply(var_dat,function(x) class(x)[1]))
+
+  if (length(unique(types))!=1) {
+    warning(paste0('All variables to be combined must be the same type ',newVarName,' not created.') )
+  } else if (types[1]=='factor') {
+    factorLevels = try(importCodes(dictTable[["Levels"]][dictTable[['VariableName']]==varsToCombine[1]]),silent = T)
+    responseIn <- unlist(lapply(factorLevels,function(x) responseVal %in%  x))
+    if (sum(responseIn)==0) {
+      warning(paste0('The response value ',responseVal,' is not a response to the variable to be combined. ',newVarName,' not created.') )
+    } else {
+      # check if the response is in var_dat or if the values and codes need to be swapped
+      if (!any(unlist(lapply(var_dat, function(x) responseVal %in% x)))){
+        need <- names(responseIn)[responseIn]
+        if (need=='code'){
+          new_dat <-NULL
+          for (j in 1:ncol(var_dat)){
+            x <- var_dat[[j]]
+            for (i in 1:nrow(factorLevels)) {
+              x <- gsub(factorLevels$label[i],factorLevels$code[i],x)
+            }
+            new_dat <-cbind(new_dat,x)
+          }
+          var_dat=as.data.frame(new_dat)
+        } else {
+          new_dat <-NULL
+          for (j in 1:ncol(var_dat)){
+            x <- var_dat[[j]]
+            for (i in 1:nrow(factorLevels)) {
+              x <- gsub(factorLevels$code[i],factorLevels$label[i],x)
+            }
+            new_dat <-cbind(new_dat,x)
+          }
+          var_dat=as.data.frame(new_dat)
+        }
+      }
+    }
+
+    check <- unlist(lapply(var_dat,function(x) responseVal %in%  x))
+    if (!any(check)){
+      warning(paste0('The response value ',responseVal,' was not found in the variables to combine. ',newVarName,' not created.') )
+    } else{
+      m <- as.matrix(var_dat)
+      m[m!=responseVal] <- NA
+      m[m==responseVal] <- 1L
+      m[is.na(m)] <- 0L
+      # row combine
+      combinedVar <- apply(m,1,function(x) paste(x,collapse=''))
+
+      # Add the variable to the data
+      data[[newVarName]] <- combinedVar
+
+      # give it an attribute so it can be plotted
+      attributes(data[[newVarName]])$graphType = 'pareto'
+      WriteToLog(paste('New combined variables created: ',newVarName,' from ',paste(varsToCombine,collapse=' to ')))
+    }
+  }
   return(data)
 }
 
@@ -649,38 +763,75 @@ WriteToLog <- function(msg,append=T,timestamp=F){
 #' This function should be run as the final step after the data have been
 #' imported, checked and the factor variables created.
 #' @param data is a data frame containing the variables to be plotted
+#' @param dictTable optional, the data dictionary returned by importExcelData or readDataDict functions to provide plot titles
 #' @param IDvar an optional string indicating the name of an identifying variable to highlight outliers
 #' @param vars is an optional character vector of the names of variables to plot
-#' @param nAsBar the number of unique values to display as a bar plot for non-factor data, default is 6
+#' @param showOutliers boolean, should outliers be labelled
+#' @param nOut integer only used if showOutliers=TRUE the most extreme nOut values are highlighted, takes precedent over qOut
+#' @param qOut proportion between 0 and 1 only used if showOutliers=TRUE the most extreme qOut values are highlighted
 #' @import ggplot2
 #' @importFrom scales dateformat
 #' @export
-plotVariables<-function(data,IDvar,vars,nAsBar=6){
+plotVariables<-function(data,vars,dictTable,IDvar,nAsBar=6,showOutliers=FALSE,nOut=NULL,qOut=.05){
   varTypes = sapply(data,function(x) class(x)[1])
   if (missing(vars)){
-    vars = names(data)[varTypes!='character']
+    char_vars = names(data)[varTypes=='character']
+    paretoVars <- NULL
+    for (v in char_vars) if (!is.null(attributes(data[[v]])$graphType)) if (attributes(data[[v]])$graphType=='pareto') paretoVars <- c(paretoVars,v)
+    vars <- c(names(data)[!varTypes=='character'],paretoVars)
   }
   N = nrow(data)
   plots <- NULL
   # Check that supplied variables are not character - if characters are found issue warning
   for (v in vars) {
-    if ('character' %in% class(data[[v]])) warning(paste(v,'is a character variable and will not be plotted. Convert to factor for plotting'))
-
+    if ('character' %in% class(data[[v]])) {
+      if (is.null(attributes(data[[v]])$graphType))  warning(paste(v,'is a character variable and will not be plotted. Convert to factor for plotting'))
+    }
     # For each variable choose an appropriate plot based on sample size and variable type and save to the list
 
-
-    if ('factor' %in% class(data[[v]]) | 'integer' %in% class(data[[v]]) ){
+    if ('character' %in% class(data[[v]])){
+      patterns <- sort(table(data[[v]]),decreasing = T)
+      npat <- min(10,length(patterns))
+      data[[v]] <- factor(data[[v]],levels = names(patterns)[1:10])
+      p <- ggplot2::ggplot(data=subset(data,!is.na(data[[v]])),aes(x=.data[[v]])) +
+        geom_bar() +
+        theme_bw() +
+        annotate('text',x=Inf,y=Inf,label=paste(npat,'most common patterns'),vjust=1,hjust=1,colour='red')
+    } else if ('factor' %in% class(data[[v]]) | 'integer' %in% class(data[[v]]) ){
       p <- ggplot2::ggplot(data=data,aes(x=.data[[v]])) +
         geom_bar() +
-       theme_bw()
+        theme_bw()
     } else {
       # make dots smaller for larger files
       dotsize = ifelse(N<30,1,ifelse(N<200,.5,.25))
 
       p <- ggplot2::ggplot(data=data,aes(x=.data[[v]])) +
         geom_dotplot(dotsize=dotsize) +
-         theme_bw() +
+        theme_bw() +
         theme(axis.title.y = element_blank(),axis.ticks.y=element_blank(),axis.text.y = element_blank())
+
+      if (showOutliers){
+        if (is.null(nOut)) {
+          nOut = floor(qOut*nrow(data))
+        } else{
+          qOut = nOut/nrow(data)
+        }
+        highlights <- quantile(data[[v]],probs = c(qOut/2,1-qOut/2))
+        data$highlight <- ifelse(data[[v]]<highlights[1]|data[[v]]>highlights[2],TRUE,FALSE)
+        if (missing(IDvar)) {
+          data$outLabel <- 1:nrow(data)
+        } else  if (!IDvar %in% names(data)) {
+          data$outLabel <- 1:nrow(data)
+        } else {
+          data$outLabel <- data[[IDvar]]
+        }
+
+        p <- p + geom_dotplot(data=subset(data,data$highlight),
+                              dotsize = dotsize,
+                              fill='red')
+        p <- p + ggrepel::geom_text_repel(data=subset(data,data$highlight),aes(x=.data[[v]],y=0,label=.data[['outLabel']]),position = position_nudge(y=.2))
+      }
+
     }
 
     # Show date axis a month-year for date variables
@@ -688,6 +839,10 @@ plotVariables<-function(data,IDvar,vars,nAsBar=6){
 
     # Add a time unit to the x-lab if there is one
     if (!is.null(attributes(data[[v]])$timeUnit)) p <- p + xlab(paste0(v," (",attributes(data[[v]])$timeUnit,'s)'))
+
+    # If the dictTable is provided at the description as a title
+    if (!missing(dictTable)) p <- p + ggtitle(dictTable$Description[dictTable$VariableName==v])
+
     plots[[v]] <- p
   }
 
